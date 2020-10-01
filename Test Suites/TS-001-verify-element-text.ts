1aa6a363bbb89e128babdaa1901be16eaa3a4d4c
minhhien <minhhien23110@gmail.com>
2020-10-01 10:35:53 +0700

    first commit

---
+++ Test Suites/TS-001-verify-element-text.ts
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-001-verify-element-text</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>925973b0-3c13-4d66-811c-6c90ade5e478</testSuiteGuid>
+   <testCaseLink>
+      <guid>4f5133e6-e129-4b11-9213-aea4585f389a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-001-verify-element-text-true</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d6645309-2abf-405e-a842-f2cd5534ca40</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>4ca122e2-d02a-4d2b-8a02-6af08fc5601a</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>96f2a717-0206-4703-84f1-e4381a800abc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-002-verify-element-text-fail</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b321da22-7d14-4692-9e59-c41d36f97f61</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>9d5e3467-fb43-4fb0-a1d6-7170f970380a</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e2df1c92-0ef7-41bf-acae-49f1933a2f65</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-003-Login-UserNameFail</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>9fe961e8-2b5d-4795-827f-7ca2acfd1051</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>adc31d43-591e-4d62-9c6e-27380a0e18f6</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6e1aad42-29e7-4f99-bea8-3889f1bf5467</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-004-Login_PassWordFail</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>caead378-5ca4-47ff-ba6d-6a4adad99e93</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a8a0b9b5-40eb-4229-84c0-9d01a3efe4ea</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5f20e21a-3a6c-4129-b691-459be211e421</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-005-Login-NoUserNamePassWord</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
